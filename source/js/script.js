f170a6e73bc2d2985a5b020dcb832a82590b5cb8
Marinarty <tsiganosha@yandex.ru>
2019-08-22 12:38:51 +0200

    начала стилизацию попапа Формы

--- source/js/script.js
+++ source/js/script.js
@@ -5,6 +5,17 @@ var filter = document.querySelector('.country-filter__container');
 var searchbtns = document.querySelectorAll('.buddies-search__filter-btn');
 var letterButtons = document.querySelectorAll('.country-filter__letter-btn');
 var lettersContainer = document.querySelector('.country-filter__letters');
+var chooseCountry = document.querySelector('.choose-country');
+var countrySelect = document.querySelector('.countries-select__item--empty .countries-select__choose-country');
+var chooseCountryClose = document.querySelector('.choose-country__close');
+
+countrySelect.onclick = function() {
+  chooseCountry.classList.add('choose-country--open')
+}
+
+chooseCountryClose.onclick = function() {
+  chooseCountry.classList.remove('choose-country--open')
+}
 
 menuBtn.onclick = function() {
   header.classList.toggle('header--menu-open')
