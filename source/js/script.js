87b91f39d6ecb8bb22ead0979dfd8234a97cddf5
Marinarty <tsiganosha@yandex.ru>
2019-08-22 15:25:15 +0200

    стилизован попап выбора страны для Формы на всех версиях

--- source/js/script.js
+++ source/js/script.js
@@ -9,14 +9,6 @@ var chooseCountry = document.querySelector('.choose-country');
 var countrySelect = document.querySelector('.countries-select__item--empty .countries-select__choose-country');
 var chooseCountryClose = document.querySelector('.choose-country__close');
 
-countrySelect.onclick = function() {
-  chooseCountry.classList.add('choose-country--open')
-}
-
-chooseCountryClose.onclick = function() {
-  chooseCountry.classList.remove('choose-country--open')
-}
-
 menuBtn.onclick = function() {
   header.classList.toggle('header--menu-open')
 }
@@ -60,3 +52,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
       map.geoObjects.add(placemark);
   }
 });
+
+countrySelect.onclick = function() {
+  chooseCountry.classList.add('choose-country--open')
+}
+
+chooseCountryClose.onclick = function() {
+  chooseCountry.classList.remove('choose-country--open')
+}
