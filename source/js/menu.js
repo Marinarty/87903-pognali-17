38940817c3727e7bc4d055e18f422beebdb32d70
Marinarty <tsiganosha@yandex.ru>
2019-08-24 16:43:49 +0200

    сделана работоспособность меню без джаваскрипта, сделан вариант хедера при скроллинге

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,9 @@
 var menuBtn = document.querySelector('.header__menu-btn');
 var header = document.querySelector('header');
 
+header.classList.remove('header--menu-open');
+menuBtn.classList.remove('header__menu-btn--hidden');
+
 menuBtn.onclick = function() {
   header.classList.toggle('header--menu-open')
 }
