0253d8a572b12731318c9b353a3ccefc66a49501
Marinarty <tsiganosha@yandex.ru>
2019-08-27 14:59:28 +0200

    ПерфектПиксель - мобильный каталог

--- source/js/countries-filter.js
+++ source/js/countries-filter.js
@@ -2,6 +2,17 @@ var filterBtns = document.querySelectorAll('.country-filter__btn');
 var filter = document.querySelector('.country-filter__container');
 var letterButtons = document.querySelectorAll('.country-filter__letter-btn');
 var lettersContainer = document.querySelector('.country-filter__letters');
+var countryGroups = document.querySelectorAll('.country-filter__letters');
+var groupButtons = document.querySelectorAll('.continents-list input');
+
+groupButtons.forEach(function(item, index) {
+  item.onchange = function() {
+    countryGroups.forEach(function(group) {
+      group.classList.remove('country-filter__letters--open');
+    })
+    countryGroups[index].classList.add('country-filter__letters--open');
+  }
+});
 
 for (var i = 0; i < filterBtns.length; i++ ) {
   filterBtns[i].onclick = function() {
